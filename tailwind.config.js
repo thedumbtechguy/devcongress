9f46a59e7d62f9148399a55cfd0604e743c581fd
TheDumbTechGuy <sfroelich01@gmail.com>
2024-12-30 20:19:55 +0000

    integrate assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
+const { execSync } = require('child_process');
+const plutoniumGemPath = execSync("bundle show plutonium").toString().trim();
+const plutoniumTailwindConfig = require(`${plutoniumGemPath}/tailwind.options.js`)
+const tailwindPlugin = require('tailwindcss/plugin')
+
 module.exports = {
+  darkMode: plutoniumTailwindConfig.darkMode,
+  plugins: [
+    // add plugins here
+  ].concat(plutoniumTailwindConfig.plugins.map(function (plugin) {
+    switch (typeof plugin) {
+      case "function":
+        return tailwindPlugin(plugin)
+      case "string":
+        return require(plugin)
+      default:
+        throw Error(`unsupported plugin: ${plugin}: ${(typeof plugin)}`)
+    }
+  })),
+  theme: plutoniumTailwindConfig.merge(
+    {
+    },
+    plutoniumTailwindConfig.theme
+  ),
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    `${__dirname}/app/**/*.{erb,haml,html,slim,rb}`,
+    `${__dirname}/app/assets/stylesheets/**/*.css`,
+    `${__dirname}/app/javascript/**/*.js`,
+    `${__dirname}/packages/**/app/**/*.{erb,haml,html,slim,rb}`,
+  ].concat(plutoniumTailwindConfig.content),
 }
